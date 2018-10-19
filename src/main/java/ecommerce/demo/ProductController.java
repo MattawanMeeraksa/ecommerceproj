8f78fdb47104c7b562cae6f50ce8103cc5871dcf
Unknown <mattawan.meeraksa@mail.kmutt.ac.th>
2018-10-19 12:30:17 +0700

    create method getProducById and getAll

--- src/main/java/ecommerce/demo/ProductController.java
+++ src/main/java/ecommerce/demo/ProductController.java
@@ -1,4 +1,25 @@
 package ecommerce.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable(name ="id") long id){
+        return productRepository.findById(id);
+    }
+
+    @GetMapping("/")
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
 }
